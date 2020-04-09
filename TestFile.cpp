b5df3668221fec58fc228f0f4683526597353805
vipilpalisakkottu <62696603+vipilpalisakkottu@users.noreply.github.com>
2020-04-09 19:37:53 +0530

    Create TestFile.cpp

---
+++ TestFile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int i = 0;
+  cout << "Enter a no" << endl;
+  cin >> i;
+  
+  if(0 == i)
+  {
+    cout << "Entered No is ZERO" << endl;
+  }
+  return 0;
+}
